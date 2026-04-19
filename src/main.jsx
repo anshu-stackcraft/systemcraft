d1f3eb8b4c45a26c89cdf1a6333873ac4db7c1a7
anshu <anshu.stackcraft@gmail.com>
2026-04-19 10:33:45 +0530

    mata

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './style/index.css'
+import './index.css'
 import App from './utils/App'
 
 createRoot(document.getElementById('root')).render(
